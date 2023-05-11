3f0a03422b696993ae640c99bd28fbf3ed49b4b2
OUISSAL EL HICHOU <114773271+ouissalhichou1@users.noreply.github.com>
2023-05-11 15:48:21 +0100

    -

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,7 +60,8 @@ class AuthController extends Controller
         ]);
     }
     public function register(Request $request){
-
+ 
+        
         $request->validate([
             'name' => 'required|string|max:255',
             'surname' =>'required|string|max:255',
